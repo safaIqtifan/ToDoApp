8f2d87c7edb11e88c3a9fc9a8e4ad035e5d68e02
Hp <safa20092017@hotmail.com>
2026-02-19 16:28:21 +0200

    commit5

---
+++ app/src/main/java/com/altaelimia/todoapp/TabChangedEvent.java
@@ -0,0 +1,9 @@
+package com.altaelimia.todoapp;
+
+public class TabChangedEvent {
+    public int tabPosition;
+
+    public TabChangedEvent(int tabPosition) {
+        this.tabPosition = tabPosition;
+    }
+}
